30df555a05c18cb616ed9d3ff98c9cc63da5332f
daw <manu17pc@gmail.com>
2026-09-21 09:13:48 +0200

    Practice2 finished and modifications in Practice2

--- practice1.py
+++ practice1.py
@@ -2,10 +2,10 @@ keepAsking = True
 
 while keepAsking == True:
 	try:
-		number1 = int(input("Introduce a number:"))
-		number2 = int(input("Introduce another number:"))
+		number1 = int(input("Introduce an integer number:"))
+		number2 = int(input("Introduce another integer number:"))
 		print("La suma de ambos número es:", number1 + number2)
 		keepAsking = False
 	except ValueError:
 
-		print("Error: Caracter introduced wasn't a number.")
+		print("Error: Caracter introduced wasn't an integer number.")
---
+++ practice2.py
@@ -0,0 +1,11 @@
+keepAsking = True
+
+while keepAsking == True:
+	try:
+		number1 = float(input("Introduce a number:"))
+		number2 = float(input("Introduce another number:"))
+		print("La suma de ambos número es:", number1 + number2)
+		keepAsking = False
+	except ValueError:
+
+		print("Error: Caracter introduced wasn't a number.")
