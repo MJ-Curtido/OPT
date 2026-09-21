598c6266363d843ee29e6c8b0a840bd16a1cc7d0
daw <manu17pc@gmail.com>
2026-09-21 09:33:43 +0200

    Practice5 finished

---
+++ practice5.py
@@ -0,0 +1,10 @@
+keepAsking = True
+
+while keepAsking == True:
+	try:
+		number1 = float(input("Introduce a number:"))
+		number2 = float(input("Introduce another number:"))
+		print("The multiplication of both numbers is:", number1 * number2)
+		keepAsking = False
+	except ValueError:
+		print("Error: Caracter introduced wasn't a number.")
